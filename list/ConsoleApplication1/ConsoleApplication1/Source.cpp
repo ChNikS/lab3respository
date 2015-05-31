db11c07f005af05d28b01d74d23ec824923c79ff
ChNikS <cherednik.nickita@yandex.ru>
2015-05-31 20:57:13 +0300

    lab3

---
+++ list/ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include<clocale>
+#include<conio.h>
+using namespace std;
+/**struct item*/
+struct item
+{
+	int data;
+	item *next;
+
+};
+/**add function*/
+void add(item *&top, int pos, int x)
+{
+	int j = 1;
+	int i; item *pnew, *p = top;
+	pnew = new item; pnew->data = x;
+	if (pos <= 1 || top == NULL)
+	{
+		pnew->next = top; top = pnew;
+	}
+	else
+	{
+		for (i = 1; i<pos - 1; i++)
+			if (p->next != NULL)
+			{
+				p = p->next;
+				j++;
+			}
+		if (pos <= j + 1)
+		{
+			pnew->next = p->next; p->next = pnew;
+		}
+	}
+}
+/**output list function*/
+void print(item *top)
+{
+	item *p = top;
+	while (p != NULL)
+	{
+		cout << p->data << ' ';
+		p = p->next;
+	}
+	cout << endl;
+}
+/**delete function*/
+void del(item *&top, int pos2)
+{
+	item *a = top, *b;
+	int i = 0;
+	while (a != NULL)
+	{
+		a = a->next;
+		i++;
+	}
+	if (pos2>i || pos2<1)
+	{
+		cout << "такого элемента нет" << endl;
+	}
+
+	else
+	{
+		if (pos2 == 1)
+		{
+			a = top->next;
+			top = a;
+		}
+		if (pos2>1)
+		{
+			i = 1;
+			a = top;
+			while (i != pos2 - 1)
+			{
+				i++; a = a->next;
+			}
+			b = a->next;
+			a->next = b->next;
+			delete b;
+		}
+	}
+}
+/**main function*/
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int pos, pos1, pos2, otv, da;
+	item *first, *last;
+	int x, r, k, m, y, j;
+	item *top = NULL;
+
+	
+
+	char action;
+	std::cout << "Введите число действий";
+	int n;
+	std::cin >> n;
+	for (int i = 0; i < n; i++){
+		std::cin >> action;
+		if (action == 'a'){
+			cout << "введите номер позиции для вставки ";
+			cin >> pos;
+			cout << "значение ";
+			cin >> x;
+			add(top, pos, x);
+		
+		
+		}
+		if (action == 'd'){
+			cout << "введите номер удаляемого элемента ";
+			cin >> pos2;
+			del(top, pos2);
+		}
+	
+	
+	}
+	
+	
+	
+	
+	print(top);
+		
+	_getch();
+}
---
+++ stek/ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -0,0 +1,63 @@
+﻿#include <string.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include<conio.h>
+
+
+
+
+
+
+void main(){
+	int mas[3];
+
+	for (int i = 0; i < 3; i++){
+		mas[i] = 0;
+	}
+	int head = -1;
+	int tail = -1;
+	int n;
+	int x = 0;
+	std::cout << "skoka?";
+	std::cin >> n;
+	char action;
+	for (int i = 0; i < n; i++){
+		std::cout << "action?";
+		std::cin >> action;
+		if (action == 'a'){
+			if (tail == -1){ head = 0; }
+			if ((tail != 2) && (mas[tail + 1] == 0)){
+				std::cout << "znach";
+				std::cin>>mas[tail + 1];
+				tail++;
+
+			}
+			if ((tail == 2) && (mas[0] == 0)){
+				std::cout << "znach";
+				std::cin>>mas[0];
+				 tail = 0;
+			}
+			for (int i = 0; i < 3; i++)std::cout << mas[i];
+
+		}
+
+		if (action == 'd'){
+			if (head == 2){ mas[head] = 0; head = 0;}
+			if (head != 2){ mas[head] = 0; head++; }
+			for (int i = 0; i < 3; i++)std::cout << mas[i];
+		
+		}
+
+	}
+
+	for (int i = 0; i < 3; i++)std::cout << mas[i];
+	_getch();
+
+}
+
+
+
+
+
